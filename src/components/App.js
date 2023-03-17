2bd058a5e35a43cf0895f8f12f9d14e09bb1d418
Rodrigo Bossini <professorbossini@gmail.com>
2023-03-17 16:57:38 -0300

    apostila07(forms-eventos-requisicoes): fim da secao 2.10
    - imagens da Pexels exibidas em lista simples, ainda sem componentes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import Busca from './Busca'
-const App = () => {
-  return (
-    <div>
-      <h1>Exibir uma lista de...</h1>
-      <Busca />
-    </div>
-  )
-}
+import env from 'react-dotenv'
+import { createClient } from 'pexels'
+export default class App extends React.Component {
+  state = {
+    pics: []
+  }
+  pexelsClient = null
+  onBuscaRealizada = (termo)  =>{
+    this.pexelsClient.photos.search({
+      query: termo
+    })
+    .then(pics => this.setState({pics: pics.photos})) 
+  }
 
-export default App
+  componentDidMount(){
+    this.pexelsClient = createClient(env.PEXELS_KEY)
+  }
+  render(){
+    return (
+      <div className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
+        <div className="col-12">
+          <h1>Exibir uma lista de...</h1>
+        </div>
+        <div className="col-8">
+          <Busca 
+            onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+        <div className="col-8">
+          {
+            this.state.pics.map((pic, key) => (
+              <div key={key}>
+                <img src={pic.src.small} alt={pic.alt} />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/Busca.js
+++ src/components/Busca.js
@@ -9,24 +9,33 @@ export class Busca extends Component {
   }
   onTermoAlterado = (event) => {
     console.log(event.target.value)
+    this.setState({termoDeBusca: event.target.value})
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    this.props.onBuscaRealizada(this.state.termoDeBusca)
   }
   render() {
     return (
-      <div className="flex flex-column">
-        {/* span.p-input-icon-left.w-full */}
-        <span className="p-input-icon-left w-full">
-          {/* i.pi.pi-search */}
-          <i className="pi pi-search"></i>
-          <InputText 
-            className='w-full'
-            onChange={this.onTermoAlterado}
-            placeholder={this.props.dica}/>
-        </span>
-        <Button 
-          label="OK"
-          className="p-button-outlined mt-2"
-        />
-      </div>
+      <form onSubmit={this.onFormSubmit}>
+        <div className="flex flex-column">
+          {/* span.p-input-icon-left.w-full */}
+          <span className="p-input-icon-left w-full">
+            {/* i.pi.pi-search */}
+            <i className="pi pi-search"></i>
+            <InputText
+              value={this.state.termoDeBusca} 
+              className='w-full'
+              onChange={this.onTermoAlterado}
+              placeholder={this.props.dica}/>
+          </span>
+          <Button 
+            label="OK"
+            className="p-button-outlined mt-2"
+          />
+        </div>
+      </form>
     )
   }
 }
